5247aa9063c45fc5b6e005435f22744524b6a135
Robert Boyd III <git@robertboyd.dev>
2022-11-23 15:16:09 -0500

    Reduce the <select /> size the right way

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -9,7 +9,7 @@
 <div
   class="flex flex-col justify-start items-start p-4 bg-slate-700 rounded-md"
 >
-  <div class="flex w-full justify-between">
+  <div class="flex w-full justify-between items-center">
     <h1
       class={`font-bold text-2xl ${cardColor}`}
       contenteditable
@@ -18,16 +18,14 @@
       {title}
     </h1>
 
-    <div class="flex flex-col">
-      <select
-        class={`bg-slate-600 rounded-md ${cardColor}`}
-        bind:value={cardColor}
-      >
-        {#each supportedColors as color}
-          <option value={color}>{color.slice(5)}</option>
-        {/each}
-      </select>
-    </div>
+    <select
+      class={`bg-slate-600 rounded-md ${cardColor}`}
+      bind:value={cardColor}
+    >
+      {#each supportedColors as color}
+        <option value={color}>{color.slice(5)}</option>
+      {/each}
+    </select>
   </div>
 
   <div class="w-full" contenteditable bind:innerHTML={body}>
