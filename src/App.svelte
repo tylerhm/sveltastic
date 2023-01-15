a3e6a15a4279498527d4a5a78e3bdc173d91bad2
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-01-15 16:38:29 -0500

    Support card adding, removing, and local storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
+  import { v4 as uuidv4 } from 'uuid';
+  import type { Card as CardType } from './components/Card';
   import Card from './components/Card.svelte';
+
+  const saveState = () => {
+    localStorage.setItem('state', JSON.stringify(cards));
+  };
+  const getState = () => {
+    return JSON.parse(localStorage.getItem('state') ?? '[]');
+  };
+
+  setInterval(() => {
+    saveState();
+  }, 1000);
+
+  let cards: Array<CardType> = getState();
+  const makeCard = () => {
+    return {
+      title: 'Card title',
+      body: 'I am a card!',
+      color: 'text-pink',
+      uuid: uuidv4(),
+    };
+  };
 </script>
 
 <svelte:head>
@@ -17,7 +40,23 @@
 
 <main class="h-screen bg-base text-text">
   <div class="w-1/3 h-40 p-10">
-    <Card title="Long title" />
+    <button
+      class="bg-rose-300 text-gray-800 rounded px-2 mb-2"
+      on:click={() => {
+        cards = [...cards, makeCard()];
+      }}>+</button
+    >
+    {#each cards as card, index}
+      <div class="mb-2">
+        <Card
+          on:removeCard={() => {
+            cards.splice(index, 1);
+            cards = cards;
+          }}
+          {card}
+        />
+      </div>
+    {/each}
   </div>
 </main>
 
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
-  export let title = 'title';
-  export let body = 'body';
+  import { createEventDispatcher } from 'svelte';
 
-  let cardColor;
+  export let card;
+
+  const dispatch = createEventDispatcher();
+
   let supportedColors = ['text-pink', 'text-teal', 'text-red', 'text-green'];
 </script>
 
 <div
   class="flex flex-col justify-start items-start p-4 bg-slate-700 rounded-md"
 >
-  <div class="flex w-full justify-between items-center">
+  <div class="flex w-full justify-end items-center">
     <h1
-      class={`font-bold text-2xl ${cardColor}`}
+      class={`font-bold text-2xl mr-auto ${card.color}`}
       contenteditable
-      bind:innerHTML={title}
+      bind:innerHTML={card.title}
     >
-      {title}
+      {card.title}
     </h1>
-
     <select
-      class={`bg-slate-600 rounded-md pl-2 ${cardColor}`}
-      bind:value={cardColor}
+      class={`bg-slate-600 rounded-md pl-2 mr-2 ${card.color}`}
+      bind:value={card.color}
     >
       {#each supportedColors as color}
         <option value={color}>{color.slice(5)}</option>
       {/each}
     </select>
+    <button
+      on:click={() =>
+        dispatch('removeCard', {
+          uuid: card.uuid,
+        })}
+    >
+      <img class="w-3" src="trash-solid.svg" alt="delete" />
+    </button>
   </div>
 
-  <div class="w-full" contenteditable bind:innerHTML={body}>
-    {body}
+  <div class="w-full" contenteditable bind:innerHTML={card.body}>
+    {card.body}
   </div>
 </div>
